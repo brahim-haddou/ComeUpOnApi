c0322aada69ef8689fd942bdea13f6556df0a8ef
EnsafCSharpProj <75618286+EnsafCSharpProj@users.noreply.github.com>
2021-01-03 09:57:16 +0100

    heroku 8

--- ComeUpOn/settings.py
+++ ComeUpOn/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -133,4 +134,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "files")
