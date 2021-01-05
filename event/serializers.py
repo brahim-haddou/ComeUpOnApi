163a21f0ef82e40d7da9e0834c25ffe9da5200dd
EnsafCSharpProj <75618286+EnsafCSharpProj@users.noreply.github.com>
2021-01-05 15:30:14 +0100

    Revert "heroku"
    This reverts commit a2959d553de43f6cfe3999824cbd5d9b3f16b736.

--- event/serializers.py
+++ event/serializers.py
@@ -1,15 +1,6 @@
 from rest_framework import serializers
 from event.models import *
 
-from django.contrib.auth.models import User
-
-
-class CurrentUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
-
-
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
@@ -17,9 +8,6 @@ class PlaceSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    address = PlaceSerializer(read_only=True)
-    user = CurrentUserSerializer(read_only=True)
-    
     class Meta:
         model = Profile
         fields = '__all__'
@@ -38,24 +26,9 @@ class ActivitySerializer(serializers.ModelSerializer):
 
 
 class EventSerializer(serializers.ModelSerializer):
-    place_event = PlaceSerializer()
-    activityEvent = ActivitySerializer(many=True)
-    
     class Meta:
         model = Event
         fields = '__all__'
-        
-    def create(self, validated_data):
-        place_event = validated_data.pop("place_event")
-        activityEvent = validated_data.pop("activityEvent")
-        
-        event = Event.objects.create(**validated_data)
-
-        Place.objects.create(**place_event)
-        for activity in activityEvent:
-            Activity.objects.create(activity)
-        
-        return event
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
